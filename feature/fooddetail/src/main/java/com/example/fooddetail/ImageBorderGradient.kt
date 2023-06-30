27445bb4ec4a830b5049bf246fef19fbb9d9b793
kim <kim@qq.com>
2023-07-01 00:15:05 +0800

    modify appearance

--- feature/fooddetail/src/main/java/com/example/fooddetail/ImageBorderGradient.kt
+++ feature/fooddetail/src/main/java/com/example/fooddetail/ImageBorderGradient.kt
@@ -1,6 +1,7 @@
 package com.example.fooddetail
 
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
 
@@ -10,9 +11,8 @@ object ImageBorderGradient {
     fun borderGradient() : List<Color> {
         val darkTheme = isSystemInDarkTheme()
         return listOf(
-            if (darkTheme) Color.White else Color.Black,
-            Color.Cyan,
-            Color.Magenta
+            if (darkTheme) Color.White else MaterialTheme.colorScheme.tertiary,
+            MaterialTheme.colorScheme.secondary
         )
     }
 }
